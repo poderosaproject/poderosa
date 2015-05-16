f19a7f96ca0e4321bd75efb3e191415dd0da1590
Iwasa Kazmi <kzmi@users.sourceforge.net>
2015-05-16 12:35:25 +0900

    update version for the next release.

--- Plugin/VersionInfo.cs
+++ Plugin/VersionInfo.cs
@@ -29,7 +29,7 @@ namespace Poderosa {
         /// Version number.
         /// </en>
         /// </summary>
-        public const string PODEROSA_VERSION = "4.3.13b";
+        public const string PODEROSA_VERSION = "4.3.14b";
         /// <summary>
         /// <ja>
         /// プロジェクト名です。
