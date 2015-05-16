1271448fd5628fc79192f6663fad0ceca027b489
Iwasa Kazmi <kzmi@users.sourceforge.net>
2015-05-16 19:23:33 +0900

    update batch file.

--- build/make-dist-src.bat
+++ build/make-dist-src.bat
@@ -13,6 +13,14 @@ RD /S /Q "%DISTBASE%\Poderosa-X.X.X"
 
 RD /S /Q "%DISTBASE%\Poderosa-X.X.X\.git"
 
+
+for /F skip^=2^ delims^=^"^ tokens^=2 %%T in ('FIND "PODEROSA_VERSION" "%DISTBASE%\Poderosa-X.X.X\Plugin\VersionInfo.cs"') do (
+  set PODEROSA_VERSION=%%T
+)
+
+RD /S /Q "%DISTBASE%\Poderosa-%PODEROSA_VERSION%"
+REN "%DISTBASE%\Poderosa-X.X.X" "Poderosa-%PODEROSA_VERSION%"
+
 pause
 
 
