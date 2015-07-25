f31965044cd542ab92f449eedc81f5346fad6732
Iwasa Kazmi <kzmi@users.sourceforge.net>
2015-07-25 13:02:14 +0900

    updae make-dist-bin.bat

--- build/make-dist-bin.bat
+++ build/make-dist-bin.bat
@@ -6,7 +6,7 @@ set DIST=%DISTBASE%\Poderosa
 set DISTCONTRIB=%DISTBASE%\ContributedPlugins
 set DOCDIST_EN=%DISTBASE%\EN\Documents
 set DOCDIST_JA=%DISTBASE%\JA\Documents
-set BINDIR=\..\bin\Release
+set BINDIR=%PROJDIR%\bin\Release
 
 RD /S /Q "%DISTBASE%"
 MD "%DISTBASE%"
@@ -15,19 +15,19 @@ MD "%DISTCONTRIB%"
 MD "%DOCDIST_EN%"
 MD "%DOCDIST_JA%"
 
-copy "%PROJDIR%\Executable%BINDIR%\Poderosa.exe" "%DIST%"
-copy "%PROJDIR%\Executable%BINDIR%\Poderosa.pdb" "%DIST%"
+copy "%BINDIR%\Poderosa.exe" "%DIST%"
+copy "%BINDIR%\Poderosa.pdb" "%DIST%"
 
-copy "%PROJDIR%\Plugin%BINDIR%\Poderosa.Plugin.dll" "%DIST%"
-copy "%PROJDIR%\Plugin%BINDIR%\Poderosa.Plugin.pdb" "%DIST%"
+copy "%BINDIR%\Poderosa.Plugin.dll" "%DIST%"
+copy "%BINDIR%\Poderosa.Plugin.pdb" "%DIST%"
 
-copy "%PROJDIR%\Granados%BINDIR%\Granados.dll" "%DIST%"
-copy "%PROJDIR%\Granados%BINDIR%\Granados.pdb" "%DIST%"
+copy "%BINDIR%\Granados.dll" "%DIST%"
+copy "%BINDIR%\Granados.pdb" "%DIST%"
 
 for %%P in (Core Macro PortForwardingCommand Protocols SerialPort TerminalEmulator TerminalSession UI Usability XZModem Pipe SFTP) do (
   MD "%DIST%\%%P"
-  copy "%PROJDIR%\%%P%BINDIR%\Poderosa.%%P.dll" "%DIST%\%%P"
-  copy "%PROJDIR%\%%P%BINDIR%\Poderosa.%%P.pdb" "%DIST%\%%P"
+  copy "%BINDIR%\Poderosa.%%P.dll" "%DIST%\%%P"
+  copy "%BINDIR%\Poderosa.%%P.pdb" "%DIST%\%%P"
 )
 
 MD "%DIST%\Protocols\Cygterm"
@@ -37,10 +37,10 @@ MD "%DIST%\Macro\Sample"
 copy "%PROJDIR%\Misc\Macro\Sample\*.js" "%DIST%\Macro\Sample"
 
 MD "%DIST%\Portforwarding"
-copy "%PROJDIR%\Portforwarding%BINDIR%\Portforwarding.exe" "%DIST%\Portforwarding"
-copy "%PROJDIR%\Portforwarding%BINDIR%\Portforwarding.pdb" "%DIST%\Portforwarding"
-copy "%PROJDIR%\Granados%BINDIR%\Granados.dll" "%DIST%\Portforwarding"
-copy "%PROJDIR%\Granados%BINDIR%\Granados.pdb" "%DIST%\Portforwarding"
+copy "%BINDIR%\Portforwarding.exe" "%DIST%\Portforwarding"
+copy "%BINDIR%\Portforwarding.pdb" "%DIST%\Portforwarding"
+copy "%BINDIR%\Granados.dll" "%DIST%\Portforwarding"
+copy "%BINDIR%\Granados.pdb" "%DIST%\Portforwarding"
 
 copy "%PROJDIR%\LICENSE.txt" "%DIST%"
 copy "%PROJDIR%\ChangeLog.txt" "%DIST%"
@@ -57,13 +57,7 @@ copy "%PROJDIR%\Doc\Help\*_ja.chm" "%DOCDIST_JA%"
 REM =====================================
 
 for %%P in ( ExtendPaste ) do (
-  MD "%DISTCONTRIB%\%%P"
-  copy "%PROJDIR%\%%P%BINDIR%\Poderosa.%%P.dll" "%DISTCONTRIB%\%%P"
-  copy "%PROJDIR%\%%P%BINDIR%\Poderosa.%%P.pdb" "%DISTCONTRIB%\%%P"
-
-  if exist "%PROJDIR%\ContributedPlugins\%%P\README*.txt" (
-    copy "%PROJDIR%\ContributedPlugins\%%P\README*.txt" "%DISTCONTRIB%\%%P"
-  )
+  xcopy "%BINDIR%\%%P" "%DISTCONTRIB%\%%P\" /S
 )
 
 pause
