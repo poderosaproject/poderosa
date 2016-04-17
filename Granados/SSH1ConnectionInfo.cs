efe4d6e66bd280b1d1e88874ac9906465518347e
Iwasa Kazmi <kzmi@users.sourceforge.net>
2016-04-17 21:12:28 +0900

    clean code

--- Granados/SSH1ConnectionInfo.cs
+++ Granados/SSH1ConnectionInfo.cs
@@ -1,10 +1,8 @@
 ﻿/*
- Copyright (c) 2005 Poderosa Project, All Rights Reserved.
+ Copyright (c) 2016 Poderosa Project, All Rights Reserved.
  This file is a part of the Granados SSH Client Library that is subject to
  the license included in the distributed package.
  You may not use this file except in compliance with the license.
-
- $Id: ConnectionInfo.cs,v 1.5 2011/11/08 12:24:05 kzmi Exp $
 */
 
 using Granados.KnownHosts;
--- Granados/SSH1Util.cs
+++ Granados/SSH1Util.cs
@@ -1,14 +1,9 @@
 ﻿/*
- Copyright (c) 2005 Poderosa Project, All Rights Reserved.
+ Copyright (c) 2016 Poderosa Project, All Rights Reserved.
  This file is a part of the Granados SSH Client Library that is subject to
  the license included in the distributed package.
  You may not use this file except in compliance with the license.
-
- $Id: SSH1Util.cs,v 1.4 2011/10/27 23:21:56 kzmi Exp $
 */
-using System;
-using Granados.IO;
-using Granados.Mono.Math;
 
 namespace Granados.SSH1 {
 }
