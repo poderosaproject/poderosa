fee3df4d91808e7ea5448a1bfc28b84a11e2b014
Iwasa Kazmi <kzmi@users.sourceforge.net>
2016-11-20 15:52:05 +0900

    disable debug trace

--- XZModem/xmodem.cs
+++ XZModem/xmodem.cs
@@ -3,7 +3,7 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 
-#define TRACE_XMODEM
+//#define TRACE_XMODEM
 
 using System;
 using System.Diagnostics;
