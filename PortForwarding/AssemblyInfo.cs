e9ea2aff1c590c5f2c8f2ca0227243b15efeba7a
Iwasa Kazmi <kzmi@users.sourceforge.net>
2017-09-03 02:09:37 +0900

    update file header.

--- PortForwarding/AssemblyInfo.cs
+++ PortForwarding/AssemblyInfo.cs
@@ -1,4 +1,18 @@
-﻿using System.Reflection;
+﻿// Copyright 2005-2017 The Poderosa Project.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System.Reflection;
 using System.Runtime.CompilerServices;
 
 //
@@ -11,7 +25,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("Copyright 2005 Poderosa Project")]
+[assembly: AssemblyCopyright("Copyright (C) 2005-2017 Poderosa Project")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
